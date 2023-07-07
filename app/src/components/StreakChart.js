44a3097cdc4c27567aaf9f65b09bf961b9da8e47
MichelleAFB <64324539+MichelleAFB@users.noreply.github.com>
2023-07-07 11:49:58 -0500

    chartjs

--- app/src/components/StreakChart.js
+++ app/src/components/StreakChart.js
@@ -3,15 +3,7 @@ import {useState,useEffect} from 'react'
 
 //outside
 
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-} from "chart.js";
+
 import { Bar } from "react-chartjs-2";
 
 import { db } from '../firebase/firebase'
